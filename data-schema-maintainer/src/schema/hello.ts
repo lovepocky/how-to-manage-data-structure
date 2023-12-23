256bf9fb566793b947ff63e33bb43424c16ad357
dadazheng <lovepocky@foxmail.com>
2023-12-23 23:04:41 +0800

    feat: add class validate

--- data-schema-maintainer/src/schema/hello.ts
+++ data-schema-maintainer/src/schema/hello.ts
@@ -1,4 +1,6 @@
-export interface SomeDataDefine {
+import { Min } from "class-validator";
+
+export abstract class SomeDataDefine {
 
 	/**
 	 * The size of the shape.
@@ -6,6 +8,7 @@ export interface SomeDataDefine {
 	 * @minimum 0
 	 * @TJS-type integer
 	 */
-	size: number;
+	@Min(0)
+	abstract size: number;
 
 }
